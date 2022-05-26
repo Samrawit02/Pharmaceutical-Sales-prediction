42aae72dc386eb6a08d094800105ac00d9c4e69b
Samrawit02 <hairaer02.gmail.com>
2022-05-26 23:56:24 +0300

    screenshots

--- scripts/config.py
+++ scripts/config.py
@@ -8,7 +8,7 @@ class Config:
   STORE_PATH = ASSETS_PATH / "data/store.csv"
   TRAIN_PATH = ASSETS_PATH / "data/train.csv"
   TEST_PATH = ASSETS_PATH / "data/test.csv"
-  DATASET_PATH = ASSETS_PATH / "data"
+  DATASET_FILE_PATH = ASSETS_PATH / "data"
   FEATURES_PATH = ASSETS_PATH / "features"
   MODELS_PATH = ASSETS_PATH / "models"
   METRICS_FILE_PATH = ASSETS_PATH / "metrics"
