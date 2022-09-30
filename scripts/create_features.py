ab33c4a8920a9882c8f4f8c704e4b73f5c858d94
Samrawit02 <hairaer02@gmail.com>
2022-09-30 16:27:02 +0300

    fixing errors

--- scripts/create_features.py
+++ scripts/create_features.py
@@ -1,83 +1,125 @@
-import os
-import sys
 import pandas as pd
 from config import Config
-from sklearn.preprocessing import LabelEncoder
-
-sys.path.append(os.path.abspath(os.path.join('../scripts')))
 from file_handler import FileHandler
+
+'''
+This is a simple script for creating features for train and test data
+I have extracted 6 features for training the model
+'''
 
 Config.FEATURES_PATH.mkdir(parents=True, exist_ok=True)
 
-file_handler = FileHandler()
 
-train_df = file_handler.read_csv(str(Config.ASSETS_PATH / "train.csv"))
-test_df = file_handler.read_csv(str(Config.ASSETS_PATH / "test.csv"))
-store_df = file_handler.read_csv(str(Config.ASSETS_PATH / "store.csv"))
+class CreateFeatures():
+  '''
+  Calculates evaluation metrics for a give model using actual data
+  '''
 
-def merge(df, store):
-    df_merge = pd.merge(df, store, on='Store')
-    return df_merge
+  def __init__(self):
+    self.train_df = pd.read_csv(str(Config.TRAIN_PATH))
+    self.test_df = pd.read_csv(str(Config.TEST_PATH))
+    self.store_df = pd.read_csv(str(Config.STORE_PATH))
+    self.file_handler = FileHandler()
 
-def get_part_of_month(day):
-    if (day < 10):
+  def get_part_of_month(self, date):
+    if (date < 10):
       return 0
-    elif(day < 20):
+    elif(date < 20):
       return 1
     else:
       return 2
 
-def extract_test_features(df):
-    df = merge(df, store_df)
+  def get_state_holiday_info(self, df):
+
     df['Date'] = pd.to_datetime(df['Date'])
-    df['Year'] = df['Date'].apply(lambda x: x.year)
-    df['Month'] = df['Date'].apply(lambda x: x.month)
-    df['DayOfMonth'] = df['Date'].apply(lambda x: x.day)
-    df['WeekOfYear'] = df['Date'].apply(lambda x: x.weekofyear)
-    df['weekday'] = df['DayOfWeek'].apply(lambda x: 0 if (x in [6, 7]) else 1)
+    df = df.sort_values('Date')
 
-    df = df[df['Open'] == 1]
-    df["part_of_month"] = df["DayOfMonth"].apply(get_part_of_month)
+    days_after_state_holiday = []
+    days_before_state_holiday = []
 
-    # since machines understand only numbers change categorical variables to numerical value
-    lb = LabelEncoder()
-    df['StateHoliday'] = lb.fit_transform(df['StateHoliday'])
-    df['Assortment'] = lb.fit_transform(df['Assortment'])
-    df['StoreType'] = lb.fit_transform(df['StoreType'])
+    num_days = 0
+    for i in df['StateHoliday']:
+      if(i == 1):
+        num_days = 0
+      else:
+        num_days += 1
+      days_after_state_holiday.append(num_days)
 
-    df = df.drop(columns=['Id', 'PromoInterval', 'Date'], axis=1)
+    small_list = []
+    bigger_list = []
+    for i in days_after_state_holiday:
+      if(i == 0):
+        small_list.reverse()
+        bigger_list.append(small_list)
+        bigger_list.append([0])
+        small_list = []
+      else:
+        small_list.append(i)
+
+    small_list.reverse()
+    bigger_list.append(small_list)
+    days_before_state_holiday = [item for sublist in bigger_list for item in sublist]
+
+    df['days_before_state_holiday'] = days_before_state_holiday
+    df['days_after_state_holiday'] = days_after_state_holiday
     return df
 
-def extract_features(df):
-    df = df['Open'] == 1
-    df["part_of_month"] = df["DayOfMonth"].apply(get_part_of_month)
+  def extract_features(self, df):
+    df = df[df['Open'] != 0]
 
-    # since machines understand only numbers change categorical variables to numerical value
-    lb = LabelEncoder()
-    df['StateHoliday'] = lb.fit_transform(df['StateHoliday'])
-    df['Assortment'] = lb.fit_transform(df['Assortment'])
-    df['StoreType'] = lb.fit_transform(df['StoreType'])
+    # convert date column to datetime
+    df = self.get_state_holiday_info(df)
 
-    df = df.drop(columns=['Sales', 'Customers', 'PromoInterval', 'Date'], axis=1)
+    # Feature creation
+    df['Year'] = df.Date.dt.year
+    df['Month'] = df.Date.dt.month
+    df['Day'] = df.Date.dt.day
+    df['DayOfWeek'] = df.Date.dt.dayofweek
+    df['WeekOfYear'] = df.Date.dt.weekofyear
+
+    df['Weekend'] = df['DayOfWeek'].apply(lambda x: 1 if x >= 6 else 0)
+    df['Weekday'] = df['DayOfWeek'].apply(lambda x: 1 if x < 6 else 0)
+
+    df["part_of_month"] = df["Day"].apply(self.get_part_of_month)
+
+    # Combining similar columns into one column and dropping old columns
+    df['CompetitionOpen'] = 12 * (df.Year - df.CompetitionOpenSinceYear) + (df.Month - df.CompetitionOpenSinceMonth)
+    df.drop(["CompetitionOpenSinceYear", "CompetitionOpenSinceMonth"], axis=1, inplace=True)
+
+    df['Promo2Open'] = 12 * (df.Year - df.Promo2SinceYear) + (df.WeekOfYear - df.Promo2SinceWeek) / 4.0
+    df.drop(["Promo2SinceYear", "Promo2SinceWeek"], axis=1, inplace=True)
+
+    df = df.set_index('Date')
+    df = df.sort_index()
     return df
 
-def extract_sales(df):
-    df = df[df['Open'] == 1]
+  def merge(self, df, store):
+    df_merge = pd.merge(df, store, on='Store')
+    return df_merge
+
+  def extract_sales(self, df):
     return df[["Sales"]]
 
-def extract_customers(df):
-    df = df[df['Open'] == 1]
+  def extract_customers(self, df):
     return df[["Customers"]]
 
+  def create_features(self):
+    train_data = self.merge(self.train_df, self.store_df)
+    train_features = self.extract_features(train_data)
+    test_data = self.merge(self.test_df, self.store_df)
+    test_features = self.extract_features(test_data)
 
-train_features = extract_features(train_df)
-test_features = extract_test_features(test_df)
+    train_sales = self.extract_sales(train_features)
+    train_customers = self.extract_customers(train_features)
 
-target_sales = extract_sales(train_df)
-target_customers = extract_customers(train_df)
+    test_features.drop(["Store", "Open"], axis=1, inplace=True)
+    train_features.drop(["Store", "Open", "Sales", "Customers"], axis=1, inplace=True)
 
-train_features.to_csv(str(Config.FEATURES_PATH / "train_features.csv"), index=None)
-test_features.to_csv(str(Config.FEATURES_PATH / "test_features.csv"), index=None)
+    self.file_handler.to_csv(test_features, str(Config.FEATURES_PATH / "test_features.csv"))
+    self.file_handler.to_csv(train_features, str(Config.FEATURES_PATH / "train_features.csv"))
+    self.file_handler.to_csv(train_sales, str(Config.FEATURES_PATH / "train_sales.csv"))
+    self.file_handler.to_csv(train_customers, str(Config.FEATURES_PATH / "train_customers.csv"))
 
-target_sales.to_csv(str(Config.FEATURES_PATH / "target_sales.csv"), index=None)
-target_customers.to_csv(str(Config.FEATURES_PATH / "target_customers.csv"), index=None)
+
+cf = CreateFeatures()
+cf.create_features()
